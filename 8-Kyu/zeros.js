defc30087eabf5b032dfc4d135459e5da75dd6ec
Regis Mucyo <mucyoregis250@gmail.com>
2026-02-16 19:59:19 +0200

    Update 8kyu kata for remove zeros

--- 8-Kyu/zeros.js
+++ 8-Kyu/zeros.js
@@ -1,5 +1,15 @@
 var noBoringZeros = function (n) {
-    return n.toString();
+    if (n < 0) {
+        var negative = Number(Math.abs(n).toString().split("").reverse().join(""));
+        return -Math.abs(Number(negative.toString().split("").reverse().join("")));
+    }
+    else {
+        return Number(Number(n.toString().split("").reverse().join(""))
+            .toString()
+            .split("")
+            .reverse()
+            .join(""));
+    }
 };
 console.log(noBoringZeros(450));
 console.log(noBoringZeros(960000));
--- 8-Kyu/zeros.ts
+++ 8-Kyu/zeros.ts
@@ -1,6 +1,16 @@
-const noBoringZeros = (n: number): any => {
-  let nu: string = n.toString().split("").reverse().join("");
-  return nu.split;
+const noBoringZeros = (n: number): number => {
+  if (n < 0) {
+    let negative = Number(Math.abs(n).toString().split("").reverse().join(""));
+    return -Math.abs(Number(negative.toString().split("").reverse().join("")));
+  } else {
+    return Number(
+      Number(n.toString().split("").reverse().join(""))
+        .toString()
+        .split("")
+        .reverse()
+        .join(""),
+    );
+  }
 };
 
 console.log(noBoringZeros(450));
