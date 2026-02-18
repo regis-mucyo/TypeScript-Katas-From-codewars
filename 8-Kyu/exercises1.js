d3c856e2677d8d7856d7dce48f798e937dabc612
Regis Mucyo <mucyoregis250@gmail.com>
2026-02-18 17:27:30 +0200

    This exercise is about learning interface and type

---
+++ 8-Kyu/exercises1.js
@@ -0,0 +1,20 @@
+// Exercise:
+var users = [
+    {
+        name: "Max Mustermann",
+        age: 25,
+        occupation: "Chimney sweep",
+    },
+    {
+        name: "Kate Müller",
+        age: 23,
+        occupation: "Astronaut",
+    },
+];
+function logPerson(user) {
+    console.log(" - ".concat(user.name, ", ").concat(user.age));
+}
+console.log(users);
+users.forEach(function (logPerson) {
+    console.log(logPerson);
+});
