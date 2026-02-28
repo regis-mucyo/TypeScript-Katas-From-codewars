a0adb049322df836331cd4e12544867f9d68de15
Regis Mucyo <mucyoregis250@gmail.com>
2026-02-28 21:37:16 +0200

    Learn abstract class part 1

---
+++ 8-Kyu/abstract.ts
@@ -0,0 +1,13 @@
+abstract class Employee {
+  constructor(
+    private firstName: string,
+    private lastName: string,
+  ) {}
+  abstract getSalary(): number;
+  get fullName(): string {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  compensationStatement(): string {
+    return `${this.fullName} makes ${this.getSalary()} a month.`;
+  }
+}
