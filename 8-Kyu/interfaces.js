e9f3eb3d70573c1adb025cf51562e2f01c6a7472
Regis Mucyo <mucyoregis250@gmail.com>
2026-03-02 20:16:51 +0200

    Revise Interface

--- 8-Kyu/interfaces.js
+++ 8-Kyu/interfaces.js
@@ -1,13 +1,20 @@
 var getsCat = function (cat) {
-    if (cat.location) {
-        return "All info are there ".concat(cat.names, ", ").concat(cat.age, " years, ").concat(cat.location, ", ").concat(cat.gender);
-    }
-    return "Hey! This is my info: ".concat(cat.names, ", ").concat(cat.age, " Years, ").concat(cat.gender);
+  if (cat.location) {
+    return "All info are there "
+      .concat(cat.names, ", ")
+      .concat(cat.age, " years, ")
+      .concat(cat.location, ", ")
+      .concat(cat.gender);
+  }
+  return "Hey! This is my info: "
+    .concat(cat.names, ", ")
+    .concat(cat.age, " Years, ")
+    .concat(cat.gender);
 };
 var cat = {
-    names: "Nyawu",
-    age: 4,
-    gender: "Female",
+  names: "Nyawu",
+  age: 4,
+  gender: "Female",
 };
 cat.names = "Black";
 console.log(getsCat(cat));
